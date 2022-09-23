505e9a3a50a8cd3a57b9d0aab6f2cc65fb5f2017
cozrodgers <coreyrodgers@gmail.com>
2022-09-23 12:00:34 +1000

    save copy of tower defense

---
+++ Assets/Pathfinding/Node.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node 
+{
+    //Pure C# Class
+    public Vector2Int coordinates;
+    public bool isWalkable;
+    public bool isExplored;
+    public bool isPath;
+    public Node connectedTo;
+    //Constructor
+    public Node(Vector2Int coordinates, bool isWalkable) {
+        this.coordinates = coordinates;
+        this.isWalkable = isWalkable;
+    }
+
+
+
+
+}
