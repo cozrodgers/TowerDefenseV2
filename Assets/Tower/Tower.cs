33da9a3b3021a901fb33d393a1c59346f28982d4
cozrodgers <coreyrodgers@gmail.com>
2022-09-24 10:14:55 +1000

    add build timer for ballista tower

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField][Range(0f,5f)] float buildDelay = 1f;
+
     [SerializeField] int cost = 75;
 
+    void Start()
+    {
+        StartCoroutine(BuildTower());
+    }
     public bool CreateTower(Tower tower, Vector3 pos)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -21,5 +27,32 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator BuildTower()
+    {
+        //turn off all children and grandchildren of meshrenderer
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+            yield return new WaitForSeconds(1f);
+
+        }
+
     }
 }
